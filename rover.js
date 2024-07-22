751523bc39ab5e37adf40f5f968a23c63efb4136
Quombus <beckettrichardmatthew@gmail.com>
2024-07-22 14:25:31 -0500

    commit 2, getting somewhere

--- rover.js
+++ rover.js
@@ -1,5 +1,11 @@
-const messageClass = require('./message');
-const commandClass = require('./command');
+const Message = require('./message');
+
+const Command = require('./command');
+
+
+let commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
+let message = new Message('Test message with two commands', commands);
+
 
 class Rover {
    // Write code here!
@@ -8,16 +14,54 @@ class Rover {
       this.mode = 'NORMAL';
       this.generatorWatts = 110;
    }
-       recieveMessage(message) {
-      let messageObject = {};
-      let messageName = message.name;
-      return messageObject.message = messageName;
-      }
-         
+       receiveMessage(message) {
       
+      let receiveMessageObject = {
+      };
+  
+
+      let results = {};
+    
+
+       for (let i = 0; i < message.commands.length; i++) { // for loop to iterate through message commands to obtain commandType and value from Command class object.
+       if (message.commands[i].commandType == 'MODE_CHANGE' && message.commands[i].value == 'LOW_POWER') { 
+        this.mode = 'LOW POWER';
+        results.completed = true;
+       }
+
+       if (message.commands[i].commandType == 'MODE_CHANGE' && message.commands[i].value == 'NORMAL') {
+        this.mode = "NORMAL";
+        results.completed = true;
+       }
+
+       if (message.commands[i].commandType == 'STATUS_CHECK') {
+        results.completed = true;
+        results.mode = this.mode;
+        results.generatorWatts = this.generatorWatts;
+        results.positon = this.position;
+       }
+
+       if (message.commands[i].commandType == 'MOVE' && this.mode =='NORMAL') {
+        this.position = message.commands[i].value;
+        results.completed = true;
+       }
+       if (message.commands[i].commandType == 'MOVE' && this.mode =='LOW POWER') {
+        results.completed = false;
+       }
+
       
 }
+return receiveMessageObject = {
+    message : message.name,
+    results
+    };
+}
 
+}
 
+let rover = new Rover(98382);    // Passes 98382 as the rover's position.
+let response = rover.receiveMessage(message);
+
+console.log(response);
 
 module.exports = Rover;
